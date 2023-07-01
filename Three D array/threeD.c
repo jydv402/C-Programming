be8badd46a0582444d0aab8153527cfa5f0af12b
jydv402 <jydv402@gmail.com>
2023-07-01 23:58:25 +0530

    Added CLI app framework

--- Three D array/threeD.c
+++ Three D array/threeD.c
@@ -1,28 +1,44 @@
 #include <stdio.h>
 void main()
 {
+    int opert;
     int i, j, a[99][99], n, m;
-    printf("Enter the array row and column: ");
-    scanf("%d %d", &m, &n);
 
-    for (i = 0; i < m; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            printf("Enter the elemt: ");
-            scanf("%d",&a[i][j]);
-        }
-    }
-
+    printf("\n\n\t1 => Normal Integer Array");
+    printf("\n\nEnter your option : ");
+    scanf("%d", &opert);
     printf("\n\n");
-    for (i = 0; i < m; i++)
+    
+    switch (opert)
     {
-        printf("|\t");
-        for (j = 0; j < n; j++)
+    case 1:
+        //Normal integer array
+        printf("Enter the array row and column: ");
+        scanf("%d %d", &m, &n);
+
+        for (i = 0; i < m; i++)
         {
-            printf("%d\t", a[i][j]);
+            for (j = 0; j < n; j++)
+            {
+                printf("Enter the elemt: ");
+                scanf("%d", &a[i][j]);
+            }
         }
-        printf("|");
-        printf("\n");
+
+        printf("\n\n");
+        for (i = 0; i < m; i++)
+        {
+            printf("|\t");
+            for (j = 0; j < n; j++)
+            {
+                printf("%d\t", a[i][j]);
+            }
+            printf("|");
+            printf("\n");
+        }
+        break;
+
+    default:
+        printf("Please selct the right option and retry!!!");
     }
 }
