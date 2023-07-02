c674a4fa77b0e99f069fc9dbf6b2767723592b00
jydv402 <jydv402@gmail.com>
2023-07-02 21:13:08 +0530

    Added Array.Play Heading

--- Three D array/threeD.c
+++ Three D array/threeD.c
@@ -3,16 +3,26 @@ void main()
 {
     int opert;
     int i, j, a[99][99], n, m;
+    printf("\n\n");
+    printf("                                    _____  _             \n");
+    printf("     /\\                            |  __ \\| |            \n");
+    printf("    /  \\   _ __ _ __ __ _ _   _    | |__) | | __ _ _   _ \n");
+    printf("   / /\\ \\ | '__| '__/ _` | | | |   |  ___/| |/ _` | | | |\n");
+    printf("  / ____ \\| |  | | | (_| | |_| | _ | |    | | (_| | |_| |\n");
+    printf(" /_/    \\_\\_|  |_|  \\__,_|\\__, |(_)|_|    |_|\\__,_|\\__, |\n");
+    printf("                           __/ |                    __/ |\n");
+    printf("                          |___/                    |___/ \n");
+    printf("\n\n\t1 => Normal Integer Array\n");
+    printf("\t2 => Character array\n");
 
-    printf("\n\n\t1 => Normal Integer Array");
     printf("\n\nEnter your option : ");
     scanf("%d", &opert);
     printf("\n\n");
-    
+
     switch (opert)
     {
     case 1:
-        //Normal integer array
+        // Normal integer array
         printf("Enter the array row and column: ");
         scanf("%d %d", &m, &n);
 
@@ -20,7 +30,7 @@ void main()
         {
             for (j = 0; j < n; j++)
             {
-                printf("Enter the elemt: ");
+                printf("Enter the element: ");
                 scanf("%d", &a[i][j]);
             }
         }
@@ -39,6 +49,6 @@ void main()
         break;
 
     default:
-        printf("Please selct the right option and retry!!!");
+        printf("Please select the right option and retry!!!");
     }
 }
